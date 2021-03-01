34d0444e2bfaa454677eb05b00633bc0a680dc1b
Alexey Kovalev <Alexey.Kovalev@kofax.com>
2021-03-01 14:12:01 +0300

    Feature: information about mestor-react-auth was added into Readme

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 import Header from "./Header";
 
 function Login(props) {
-
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
   function handlesubmit(e) {
     // Запрещаем браузеру переходить по адресу формы
@@ -15,40 +14,35 @@ function Login(props) {
   }
 
   const handleChange = (e) => {
+    e.target.name === "login__email" ? setEmail(e.target.value) : "";
 
-    e.target.name === 'login__email' ? setEmail(e.target.value) : ''
-
-    e.target.name === 'login__password' ? setPassword(e.target.value) : ''
-  }
+    e.target.name === "login__password" ? setPassword(e.target.value) : "";
+  };
 
   return (
-      <>
-         <Header 
-          linkName='Регистрация'
-          link='/mesto-react/sign-up'
-           />
-        <form 
-        className='login'
-        onSubmit={handlesubmit}>
-        <h2 className='login__label'>Вход</h2>
-        <input 
-        className='login__email'
-         placeholder='Email'
-         onChange={handleChange}
-         name='login__email'
-         />
-        <input 
-        type="password" 
-        className='login__password' 
-        placeholder='Пароль'
-        onChange={handleChange}
-        name='login__password'
+    <>
+      <Header linkName="Регистрация" link="/mesto-react/sign-up" />
+      <form className="login" onSubmit={handlesubmit}>
+        <h2 className="login__label">Вход</h2>
+        <input
+          className="login__email"
+          placeholder="Email"
+          onChange={handleChange}
+          name="login__email"
         />
-        <button type='submit' className='login__submit'>Войти</button>
-            
-    </form>
-   </> 
-    );
+        <input
+          type="password"
+          className="login__password"
+          placeholder="Пароль"
+          onChange={handleChange}
+          name="login__password"
+        />
+        <button type="submit" className="login__submit">
+          Войти
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default Login;
